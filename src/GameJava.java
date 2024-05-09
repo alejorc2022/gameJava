77e1fa4509ef42c0f8d8e82036d6f2a07ca6b7dd
alejorc2022 <arc-@outlook.com>
2024-05-09 15:09:02 -0500

    juego sencillo en java

---
+++ src/GameJava.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class GameJava {
+    public static void main(String[] args) throws Exception {
+        
+
+        //clase scanner para escribir la entrada
+          Scanner scanner = new Scanner(System.in);
+
+
+          //declaraciones y asignaciones
+          String palabraSecreta = "inteligencia";
+          int intentosMaximos = 10;
+          int intentos = 0;
+          boolean palabraAdivinada = false;
+
+
+          //arreglos
+
+          char[] letrasAdivinadas = new char[palabraSecreta.length()];
+
+          //estructura de control
+          for (int i = 0; i < letrasAdivinadas.length; i++) {
+                letrasAdivinadas[i] = '_';
+          }
+
+
+          //estructura de control iterativa
+
+          while (!palabraAdivinada && intentos < intentosMaximos) {
+            System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " (" + palabraSecreta.length() + " letras)");
+
+            System.out.println("Introduce una letra por favor");
+            char letra =  Character.toLowerCase(scanner.next().charAt(0))  ;
+
+            boolean letraCorrecta = false;
+
+            for (int i = 0; i < palabraSecreta.length(); i++) {
+                //estructura de control condicional
+                if (palabraSecreta.charAt(i)== letra) {
+
+                    letrasAdivinadas[i] = letra;
+                    letraCorrecta = true;
+                    
+                }
+            }
+
+
+            if (!letraCorrecta) {
+                intentos++;
+                System.out.println(" Incorrecto - Te quedan " + (intentosMaximos - intentos) + " intentos." );
+                
+            }
+
+
+            if (String.valueOf(letrasAdivinadas).equals(palabraSecreta)) {
+                palabraAdivinada = true;
+                System.out.println("Felicidades - has adivinado la palabra secreta: " + palabraSecreta);
+            }
+
+          }
+
+          if (!palabraAdivinada) {
+            System.out.println(" Que pena te has quedado sin intentos GAME OVER");
+            
+          }
+
+        scanner.close();
+          
+
+    }
+}
